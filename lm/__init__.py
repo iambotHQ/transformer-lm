25790367c4bbfd2b9c0f30f69b9cc5ea0d8f783b
Grzegorz <suszka.grzegorz@gmail.com>
2019-08-11 00:23:09 +0200

    add default hparams to package

---
+++ lm/__init__.py
@@ -0,0 +1,9 @@
+import json
+from pathlib import Path
+
+from .common import END_OF_LINE, END_OF_TEXT, UNK
+from .inference import ModelWrapper
+from .model import HParams, Model
+
+default_hparams = json.loads((Path(__file__).parent / "params.json").read_text())["hparams"]
+default_hparams.setdefault("n_hidden", default_hparams["n_embed"])
