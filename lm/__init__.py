2cb7737c286fb276207ef3bc64500925986f256c
Grzegorz <suszka.grzegorz@gmail.com>
2019-08-11 00:27:35 +0200

    fix hparams path

--- lm/__init__.py
+++ lm/__init__.py
@@ -5,5 +5,5 @@ from .common import END_OF_LINE, END_OF_TEXT, UNK
 from .inference import ModelWrapper
 from .model import HParams, Model
 
-default_hparams = json.loads((Path(__file__).parent / "params.json").read_text())["hparams"]
+default_hparams = json.loads((Path(__file__).parent / "default_params.json").read_text())["hparams"]
 default_hparams.setdefault("n_hidden", default_hparams["n_embed"])
